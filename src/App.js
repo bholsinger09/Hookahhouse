1763a6d999cb8bdbeab034be106d6708af7bb959
bholsinger09 <bholsinger@gmail.com>
2021-07-09 15:18:36 -0600

    got working title component in header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import CompanyTitle from '../src/Components/company_title';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CompanyTitle/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
