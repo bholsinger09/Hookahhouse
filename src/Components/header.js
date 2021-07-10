00bdf389785610f8afbc4633d59f63c5c66bc4f6
bholsinger09 <bholsinger@gmail.com>
2021-07-10 15:52:21 -0600

    updated header component has bug where components are not rendering - no render errors

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,4 +1,25 @@
 import React from 'react'
 
-import CompanyTitle from '../src/Components/company_title';
+import companyTitle from '../Components/companyTitle';
 
+const header = ()=> {
+    return(
+        <header className="App-header">
+        <companyTitle/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+    )
+};
+
+export default header;
