ca1a63439fa19a4ed6091375acacaf991d2e1daa
bholsinger09 <bholsinger@gmail.com>
2021-07-12 16:05:14 -0600

    more updates to the title

---
+++ src/Components/companyTitle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const companyTitle = () => {
+
+return (
+    <div class = "siteName">
+      <h1 id = "title">Hookah House</h1>
+    </div>
+  )
+};
+
+
+export default companyTitle;
+
