5ca33a1a47d46181efc2bc472658e32b2f0f67e7
bholsinger09 <bholsinger@gmail.com>
2021-07-14 15:36:51 -0600

    updated navMenu and companyTitle separated them.  Still more to be done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import './App.css';
 import Header from '../src/Components/header';
-import reactDom from 'react-dom';
+
 import NavBar from './Components/navBar';
 
 function App() {
