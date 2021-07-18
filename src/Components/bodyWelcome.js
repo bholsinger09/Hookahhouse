d8d685cae464ec275c279a84be9f208d9c2233d6
bholsinger09 <bholsinger@gmail.com>
2021-07-18 16:06:31 -0600

    working on homepage with products.  Header nav bar formatted.  Links still need routing.

--- src/Components/bodyWelcome.js
+++ src/Components/bodyWelcome.js
@@ -5,6 +5,8 @@ const Welcome = () => {
         <React.Fragment>
       <div>
         <h1 id="welcome">Welcome to the Grocery & Hookah house!</h1>
+        <p class="introductionPar"id="introductionProducts">Come to visit our store, we carry the very best shisha, hookah, tobacco, and vape products. </p>
+        <p class="introductionPar"id="introductionGrocery"> Come see our new Grocery products such as a varity of Tea and Coffee. </p>
 
       </div>
 
