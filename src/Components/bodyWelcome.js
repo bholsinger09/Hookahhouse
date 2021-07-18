5c7621fe5b7a3730d52c2a3a5eb9dcfb46933d4c
bholsinger09 <bholsinger@gmail.com>
2021-07-18 15:43:23 -0600

    added component for bodyWelcom

---
+++ src/Components/bodyWelcome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <React.Fragment>
+      <div>
+        <h1 id="welcome">Welcome to the Grocery & Hookah house!</h1>
+
+      </div>
+
+        </React.Fragment>
+        
+
+    )
+
+};
+
+export default Welcome;
