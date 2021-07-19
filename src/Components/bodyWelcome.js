cb3f20cadb062dafcfd9b7aa417d685d05ab48be
bholsinger09 <bholsinger@gmail.com>
2021-07-19 16:52:02 -0600

    added free shipping and working more on nav to get it it to stick

--- src/Components/bodyWelcome.js
+++ src/Components/bodyWelcome.js
@@ -7,6 +7,7 @@ const Welcome = () => {
         <h1 id="welcome">Welcome to the Grocery & Hookah house!</h1>
         <p class="introductionPar"id="introductionProducts">Come to visit our store, we carry the very best shisha, hookah, tobacco, and vape products. </p>
         <p class="introductionPar"id="introductionGrocery"> Come see our new Grocery products such as a varity of Tea and Coffee. </p>
+        <p class="introductionPar"id="introductionFreeShipping"> Free shipping for all Products to all US States</p>
 
       </div>
 
--- src/Components/navBar.js
+++ src/Components/navBar.js
@@ -5,7 +5,8 @@ const NavBar = () => {
     return(
         <React.Fragment>
         
-      <div class="navBar">
+      <nav class="navBar" class="sticky" >
+        <div id="nav">
       <CompanyTitle/>
         <ul>
   <li class="navMenu"id="homeMenu" ><a href="default.asp">Home</a></li>
@@ -14,6 +15,7 @@ const NavBar = () => {
   <li class="navMenu" id="directionsMenu"><a href="#top">Get Directions</a></li>
        </ul> 
        </div>
+       </nav>
        </React.Fragment>
 
     )
