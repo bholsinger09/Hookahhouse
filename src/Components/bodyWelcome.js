c8cfa017fcf287f01fdd4452947a6dc3978c779d
bholsinger09 <bholsinger@gmail.com>
2021-07-20 11:51:02 -0600

    nav is set to fixed position cleaned up not needed divs

--- src/Components/bodyWelcome.js
+++ src/Components/bodyWelcome.js
@@ -5,9 +5,9 @@ const Welcome = () => {
         <React.Fragment>
       <div>
         <h1 id="welcome">Welcome to the Grocery & Hookah house!</h1>
-        <p class="introductionPar"id="introductionProducts">Come to visit our store, we carry the very best shisha, hookah, tobacco, and vape products. </p>
-        <p class="introductionPar"id="introductionGrocery"> Come see our new Grocery products such as a varity of Tea and Coffee. </p>
-        <p class="introductionPar"id="introductionFreeShipping"> Free shipping for all Products to all US States</p>
+        <p className="introductionPar"id="introductionProducts">Come to visit our store, we carry the very best shisha, hookah, tobacco, and vape products. </p>
+        <p className="introductionPar"id="introductionGrocery"> Come see our new Grocery products such as a varity of Tea and Coffee. </p>
+        <p className="introductionPar"id="introductionFreeShipping"> Free shipping for all Products to all US States</p>
 
       </div>
 
--- src/Components/navBar.js
+++ src/Components/navBar.js
@@ -5,16 +5,16 @@ const NavBar = () => {
     return(
         <React.Fragment>
         
-      <nav class="navBar" class="sticky" >
-        <div id="nav">
+      <nav id="nav">
+        
       <CompanyTitle/>
         <ul>
-  <li class="navMenu"id="homeMenu" ><a href="default.asp">Home</a></li>
-  <li class="navMenu" id= "quoteMenu"><a href="#top">Get Quote</a></li>
-  <li class="navMenu" id="contactMenu"><a href="#top">Contact Us</a></li>
-  <li class="navMenu" id="directionsMenu"><a href="#top">Get Directions</a></li>
+  <li className="navMenu"id="homeMenu" ><a href="default.asp">Home</a></li>
+  <li className="navMenu" id= "quoteMenu"><a href="#top">Get Quote</a></li>
+  <li className="navMenu" id="contactMenu"><a href="#top">Contact Us</a></li>
+  <li className="navMenu" id="directionsMenu"><a href="#top">Get Directions</a></li>
        </ul> 
-       </div>
+       
        </nav>
        </React.Fragment>
 
