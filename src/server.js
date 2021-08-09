687ea0d24838c15e919cca07c01afe4c609db1d6
bholsinger09 <bholsinger@gmail.com>
2021-08-09 13:36:24 -0600

    getting post to work.  Showing undefined response

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
 
+
 const app = express();
 
-app.get('/hello', (req,res) => res.send('Hello back'));
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (req,res) => res.send('Hello back'));
+app.post('/' , (req,res) => res.send(`data ${req.body.name}`));
 
 app.listen(8000, ()=> console.log(' Listening on port 8000'));
