69570940e6209f3cd339ce783d3fe14af9a7dc9b
bholsinger09 <bholsinger@gmail.com>
2021-08-10 17:28:31 -0600

    working more on the server side.  Mysql is next.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,22 @@
 import express from 'express';
+//import bodyParcer from 'body-parser';
+
 
 
 const app = express();
 
+
+//depreciated
+//app.use(bodyParcer.json());
 
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get('/', (req,res) => res.send('Hello back'));
-app.post('/' , (req,res) => res.send(`data ${req.body.name}`));
+//app.post('/' , (req,res) => res.send(`${req.body.name}`));//this results in undefined
+app.post('/' , (req,res) => res.send(req.body.name)); // this results in :  1 and req.body results in {} empty object
+
+
 
 app.listen(8000, ()=> console.log(' Listening on port 8000'));
