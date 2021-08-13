6eb31c4aa83633a0e669f8a00e9c0390af32a4cc
bholsinger09 <bholsinger@gmail.com>
2021-08-13 13:10:49 -0600

    submit button function added for usestate.  Bug on change is constantly called.  Submit returns empty string

--- src/Pages/getQuote.js
+++ src/Pages/getQuote.js
@@ -9,19 +9,42 @@ const GetQuote = (props) => {
   const [email, setEmail] = useState("");
   const [question, setQuestion] = useState("");
 
+  const dispFields = () => {
+    console.log(name + email + question);
+    //console.log("test");
+  };
+  
+
+  /*
+  This is used in case prevent defualt needs used 
+function handleSubmit(e) {
+e.preventDefault();    }
+  
+
+ */
+
+  
+
+
+
     return(
         <React.Fragment>
-    
 
-    <form id="quoteForm" >
+
+
+    <form id="quoteForm" 
+    //onSubmit={handleSubmit}
+    >
       <h1 id="quoteTitle">Quote Help Form</h1>
       <p id="quotePar">Please provide your Name, Contact Email, and what products you would like more information about in this form :</p>
 
   <label id="formName" className="Form">
     Name:
     <input type="text" name="name" 
-    onChange={(event) => { setName(event.target.value);
-    }}/>
+
+    onChange={(event) => { setName(event.target.value);}}
+
+    />
   </label>
   
   <label id="formEmail" className="Form">
@@ -41,7 +64,13 @@ const GetQuote = (props) => {
   <br/>
   <br/>
 
-  <input id="quoteSubmit"type="submit" value="Submit" />
+  <button id="quoteSubmit" type="submit" 
+
+  //funtion is called however seems to constantly call the useState which is used in onchange
+  //when submit is done returns empty string 
+
+  onClick = {dispFields()}
+  >Submit </button>
 
   </form>
 
