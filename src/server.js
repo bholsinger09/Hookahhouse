b17f0e91c01e73dbbcab935003d4b0376e03c6a7
bholsinger09 <bholsinger@gmail.com>
2021-08-18 13:32:27 -0600

    added sql backend post (not tested with front end yet)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,32 @@
 const express = require("express");
 
+const mysql = require("mysql");
+
+const db =  mysql.createConnection({
+    user:'root',
+    host: 'localhost',
+    password:'D5q%9tY5jfiW',
+    database:'hookahsite'
+})
+
 const app = express();
 
-app.get('/hello', (req,res) => res.send('Hello back'));
+app.post(('/create'), (req,res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const question = req.body.question;
+
+    db.query("INSERT INTO cust_questions (name, email, question) VALUES (?,?,?)",
+    [name, email, question], (err,result)=> {
+        if (err) {
+            console.log(err)
+        }else {
+            res.send('data sent')
+        }
+    }
+    );
+} );
+
+app.get('/', (req,res) => res.send('This is a get request'));
 
 app.listen(8000, ()=> console.log(' Listening on port 8000'));
